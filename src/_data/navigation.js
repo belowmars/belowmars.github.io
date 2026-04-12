18eac158bff04cc96971da854038891c1160e638
belowmars <46431118+belowmars@users.noreply.github.com>
2026-04-12 18:23:19 +0200

    Update navigation.js

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -1,5 +1,5 @@
 module.exports = [
   { name: "Home", url: "/" },
-  { name: "Mission Logs", url: "/logs/" },
-  { name: "Begin Descent", url: "/descent/" }
+  { name: "Mission Logs", url: "/logs.njk" },
+  { name: "Begin Descent", url: "/descent.njk" }
 ];
